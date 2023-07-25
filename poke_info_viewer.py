05f63daa8c9a9be175d3a491f608f5218720dcc4
DeepMahida07 <DeepMaheshbhai.Mahida@flemingcollege.ca>
2023-07-25 11:53:18 -0400

    Edited the file

--- poke_info_viewer.py
+++ poke_info_viewer.py
@@ -16,10 +16,10 @@ root.title("Pokemon Information")
 
 # TODO: Create the frames
 frm_top = ttk.Frame(root)
-frm_top.grid(row=0, column=0, columnspan=2)
+frm_top.grid(row=0, column=3, columnspan=2)
 
 frm_btm_left = ttk.Frame(root)
-frm_btm_left.grid(row=1, column=0)
+frm_btm_left.grid(row=1, column=1)
 frm_btm_right = ttk.Frame(root)
 frm_btm_right.grid(row=1, column=1)
 
@@ -38,19 +38,35 @@ ent_name.grid(row=0, column=1, padx=(20,5), pady=(20,20))
 frm_input = ttk.Label(root, text=('Info'))
 frm_input.grid(row=1, column=0, padx=(20,10), pady=(10,20))
 
+#Getting info table 
+enter_name = ttk.Entry(frm_top)
+enter_name.insert(0, 'meow')
+enter_name.grid(row=0, column=1)
+
+def handle_button_get_info():
+    poke_name = enter_name.get().strip()
+    if poke_name == '': return
+    poke_info = get_pokemon_info(poke_name)
+    if poke_info:
+        label_height_value = [text] = str(poke_info['Height']) + ' dm'
+        label_weight_value = [text] = str(poke_info['Weight']) + ' hg'
+        
+label_height = ttk.Label(frm_input, text='Height:')
+label_height.grid(row=0, column=0, padx=(10,5), pady=(10,5), sticky=E)
+
+label_height_value = ttk.Label(frm_input, width=20)
+label_height_value.grid(row=1, column=1, padx=(0,10), pady=(10,5), sticky=W)
+
+
+
+
+
+
+
 frm_input = ttk.Label(root, text=('Stat'))
 frm_input.grid(row=1, column=1, padx=(20,10), pady=(10,20))
 
 # TODO: Define button click event handler function
-
-
-
+get_button = ttk.Button(frm_top, text='Get Info')
+get_button.grid(row=0, column=2, padx=10, pady=5)
 root.mainloop()
-
-
-#enter_name = ttk.Entry(frm_top)
-#enter_name.insert(0, 'meow')
-#enter_name.grid(row=0, column=1)
-#def handle_button_get_info():
-    #poke_name = enter_name.get().strip()
-    #if poke_name = 

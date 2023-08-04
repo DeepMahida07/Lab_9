0b1be86b7d3d5ac581e84ce06bf7926295798dc1
DeepMahida07 <DeepMaheshbhai.Mahida@flemingcollege.ca>
2023-08-04 10:02:40 -0400

    Edited the file

--- poke_info_viewer.py
+++ poke_info_viewer.py
@@ -35,8 +35,11 @@ lbl_name.grid(row=0, column=0, padx=15, pady=20)
 ent_name = ttk.Entry(frm_top)
 ent_name.grid(row=0, column=1, padx=(20,5), pady=(20,20))
 
-frm_input = ttk.Label(root, text=('Info'))
-frm_input.grid(row=1, column=0, padx=(20,10), pady=(10,20))
+frm_INFO = ttk.Label(root, text=('Info'))
+frm_INFO.grid(row=1, column=0, padx=(20,10), pady=(10,20))
+
+frm_STAT = ttk.Label(root, text=('Stat'))
+frm_STAT.grid(row=1, column=1, padx=(20,10), pady=(10,20))
 
 #Getting info table 
 enter_name = ttk.Entry(frm_top)
@@ -51,24 +54,20 @@ def handle_button_get_info():
         label_height_value = [text] = str(poke_info['Height']) + ' dm'
         label_weight_value = [text] = str(poke_info['Weight']) + ' hg'
         
-label_height = ttk.Label(frm_input, text='Height:')
+label_height = ttk.Label(frm_INFO, text='Height:')
 label_height.grid(row=3, column=0, padx=(10,5), pady=(10,5), sticky=E)
-label_height_value = ttk.Label(frm_input, width=20)
+label_height_value = ttk.Label(frm_INFO, width=20)
 label_height_value.grid(row=1, column=1, padx=(0,10), pady=(10,5), sticky=W)
 
-#label_weight = ttk.Label(frm_input, text='Weight:')
-#label_weight.grid(row=5, column=0, padx=(10,5), pady=(10,5), sticky=E)
-#label_weight_value = ttk.Label(frm_input, width=20)
-#label_weight_value.grid(row=3, column=1, padx=(0,10), pady=(10,5), sticky=W)
+#label_weight = ttk.Label(frm_INFO, text='Weight:')
+#label_weight.grid(row=5, column=0, padx=(10,5), pady=(10,5), sticky=S)
+#label_weight_value = ttk.Label(frm_INFO, width=20)
+#label_weight_value.grid(row=3, column=1, padx=(0,10), pady=(10,5), sticky=E)
 
-
-
-
-
-frm_input = ttk.Label(root, text=('Stat'))
-frm_input.grid(row=1, column=1, padx=(20,10), pady=(10,20))
+label_HP = ttk.Label(frm_STAT, text='HP:')
+label_HP.grid(row=3, column=1, padx=(10,5), pady=(10,5), sticky=E)
 
 # TODO: Define button click event handler function
 get_button = ttk.Button(frm_top, text='Get Info')
-get_button.grid(row=0, column=2, padx=10, pady=5)
+get_button.grid(row=0, column=2, padx=10, pady=10)
 root.mainloop()
